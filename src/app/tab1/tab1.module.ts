edbc74f85ace6d8a49a14bc97a49cd350613d4fc
PipeX3m <pipex3m@localhost.localdomain>
2020-08-18 13:55:43 -0400

    rueba de commit sd

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
+
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
@@ -17,4 +18,7 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
   ],
   declarations: [Tab1Page]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule {
+
+
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { StreamingMedia, StreamingAudioOptions, StreamingVideoOptions } from '@ionic-native/streaming-media/ngx';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { observable } from 'rxjs';
+import { BackgroundMode } from '@ionic-native/background-mode/ngx';
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +11,93 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  errorShow: String = "";
+  url: string;
+  stream: any;
+  promise: any;
+  isPlaying: Boolean;
+  constructor(public streamingMedia: StreamingMedia, private localNotifications: LocalNotifications, private backgroundMode: BackgroundMode) {
+    //this.url = "http://rv3.denialstream.com:8074/;stream.mp3";
+    this.url = "http://rv3.denialstream.com:8074/;stream.mp3";
+    this.stream = new Audio(this.url);
+    this.isPlaying = false;
 
-  constructor() {}
+
+
+  }
+
+  playMusicRadio() {
+    //this.backgroundMode.enable();
+    this.isPlaying = true;
+    this.stream.load();
+    console.info(this.stream);
+    this.errorShow = "antes del play";
+
+    try {
+      this.stream;
+      this.stream.volume = 1;
+      this.promise = this.stream.play();
+      this.promise.then(function (response) { }, function (err) {
+        alert(err);
+      });
+
+    } catch (exception) {
+      alert(exception);
+    }
+
+
+    // Schedule a single notification
+    this.localNotifications.schedule({
+      id: 1,
+      text: 'Escuchando FMSombras.',
+      sticky: true,
+      actions: [
+        
+        { id: 'apagar', title: 'Apagar' }
+      ]
+    });
+    this.localNotifications.on("apagar").subscribe(() => {
+      this.stopMusic();
+
+    });
+    
+  }
+  stopMusic() {
+    this.stream.pause();
+    this.localNotifications.update({ id: 1, sticky: false });
+    this.isPlaying = false;
+    this.localNotifications.clearAll();
+    //this.backgroundMode.disable();
+  }
+
+  playMusic() {
+    this.stream.load();
+    try {
+      this.stream.volume = 1;
+      this.promise = this.stream.play();
+      this.promise.then(function (response) { }, function (err) {
+        alert(err);
+      });
+
+    } catch (exception) {
+      alert(exception);
+    }
+  }
+
+  openSocial(social:any){
+    if(social===1){
+      window.open("https://twitter.com/fansitecl/");
+    }
+    if(social===2){
+      window.open("https://www.facebook.com/fansite.cl/");
+    }
+    if(social===3){
+      window.open("https://www.instagram.com/fansitecl/");
+    }
+    if(social===4){
+      window.open("https://www.youtube.com/user/Fansitetv");
+    }
+  }
+
 
 }
